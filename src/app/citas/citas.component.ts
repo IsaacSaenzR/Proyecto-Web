24b9576b1ec08b0f14d381fca70a165e7631090e
Alexia Cruces Avila <AlexiaCrucesAvila29@gmail.com>
2024-06-25 08:19:32 -0600

    ALV

--- src/app/citas/citas.component.ts
+++ src/app/citas/citas.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { EventosAService } from '../Services/eventos-a.service';
 
 @Component({
   selector: 'app-citas',
@@ -8,5 +9,27 @@ import { Component } from '@angular/core';
   styleUrl: './citas.component.scss'
 })
 export class CitasComponent {
+  public events: any;
+  public datos: any;
 
+  constructor(private eventbriteService: EventosAService) { 
+    var usu = sessionStorage.getItem("usuario")
+    this.datos["nombredeusuario"] = JSON.parse(usu?usu:"")
+  }
+
+  ngOnInit(): void {
+    this.eventbriteService.miLectura(this.datos.nombredeusuario, "").subscribe(
+      (response: any) => {
+        this.events = response;
+        console.log(this.events);
+      },
+      (error: any) => {
+        console.error('Error fetching events:', error);
+      }
+    );
+  }
+
+  eliminar(id:string){
+    this.eventbriteService.eliminar(this.datos.nombredeusuario, id)
+  }
 }
